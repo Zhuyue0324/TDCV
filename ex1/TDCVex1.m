ac5e4488c2f9811e27704bf5ac637e2eb9aba4d6
ThomaLa <thomas.lapotre@gmail.com>
2017-11-23 15:13:49 +0100

    Change paths to be relative

--- ex1/TDCVex1.m
+++ ex1/TDCVex1.m
@@ -1,9 +1,9 @@
 %Task1
-init1=imread('E:/TDCVex1/data/images/init_texture/DSC_9743.jpg');
-%init2=imread('E:/TDCVex1/data/images/init_texture/DSC_9744.jpg');
+init1=imread('../data/images/init_texture/DSC_9743.jpg');
+%init2=imread('../data/images/init_texture/DSC_9744.jpg');
 imshow(init1)
 %imshow(init2)
-%teabox3d=pcread('E:/TDCVex1/data/model/teabox.ply')
+%teabox3d=pcread('../data/model/teabox.ply')
 %pcshow(teabox3d)
 print("over")
     
