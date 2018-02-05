f48e1af6ef5d6c0886514c21d9ffa89a27ec8575
ThomaLa <thomas.lapotre@gmail.com>
2018-02-05 20:02:17 +0100

    ipynb loads the datasets

--- ex3/props.py
+++ ex3/props.py
@@ -1,2 +1,3 @@
 # properties paths
-DATA_ROOT = 'C:/Users/thomala/Documents/TDCV-misc/ex3/dataset'
+DATA_ROOT = 'C:/Users/thomala/Documents/TDCV-misc/ex3/dataset/'
+DATA_PREP = 'C:/Users/thomala/Documents/TDCV-misc/ex3/preprocessing/'
