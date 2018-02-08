fb8e7ec20ef0140f495f1138c635e0ea919bd3e1
ThomaLa <thomas.lapotre@gmail.com>
2018-02-08 11:58:42 +0100

    IPYNB refactored

--- ex3/props.py
+++ ex3/props.py
@@ -1,3 +1,4 @@
 # properties paths
 DATA_ROOT = 'C:/Users/thomala/Documents/TDCV-misc/ex3/dataset/'
 DATA_PREP = 'C:/Users/thomala/Documents/TDCV-misc/ex3/preprocessing/'
+WEIGHTS = 'C:/Users/thomala/Documents/TDCV-misc/ex3/weights/model.ckpt'
