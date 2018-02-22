f18b300cab8abbc2b83f3d263c84b4336a840ebf
ThomaLa <thomas.lapotre@gmail.com>
2018-02-22 15:43:32 +0100

    removed useless py and updated ipynb outputs

--- ex3/props.py
+++ ex3/props.py
@@ -2,3 +2,4 @@
 DATA_ROOT = 'C:/Users/thomala/Documents/TDCV-misc/ex3/dataset/'
 DATA_PREP = 'C:/Users/thomala/Documents/TDCV-misc/ex3/preprocessing/'
 WEIGHTS = 'C:/Users/thomala/Documents/TDCV-misc/ex3/weights/model.ckpt'
+LOGS = 'C:/Users/thomala/Documents/TDCV-misc/ex3/logs/'
